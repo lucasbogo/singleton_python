c9741f9a44f254a64587da1305e42d15e6408e4d
Lucas Abreu Bogo <lucas@lucas-abreu-bogo>
2022-10-24 01:52:13 -0300

    removed 'any'

--- singleton_metaclass.py
+++ singleton_metaclass.py
@@ -6,13 +6,15 @@ class Singleton(type):
 
     # Call method> Check whether or not the class is in the set of instances
     # If it is not, then we construct it and subsequently return it
-    def __call__(cls, *args: Any, **kwds):
+    def __call__(cls, *args, **kwds):
+        # Check if cls is in instance
         if cls not in cls._instances:
+             # If it is not, then we construct it
             cls._instances[cls] = super(Singleton, cls)\
-                .__call__(*args, **kwargs)
-        return cls._instances[cls]
+            .__call__(*args, **kwargs) # Call the initializer with the arguments and keyword arguments
+        return cls._instances[cls] # Then return instances
 
-
+# Use the metaclass singleton
 class Database(metaclass=Singleton):
     def __init__(self):
         print('Loading database')
